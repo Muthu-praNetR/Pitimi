7c4179dc601f346a6067f1a2a5d91cde9b460b28
Rubens Mariuzzo <rubens@mariuzzo.com>
2015-12-24 12:07:59 -0400

    Feature: Spanish language added.

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+
+    'app_name'        => 'Pitimi',
+    'app_description' => 'Gestor de Discursos Públicos',
+
+    'login'  => 'Ingresar',
+    'logout' => 'Salir',
+
+    'email'    => 'Correo Electrónico',
+    'password' => 'Contraseña',
+
+    'speakers'      => 'Oradores',
+    'talks'         => 'Bosquejos',
+    'congregations' => 'Congregaciones',
+    'users'         => 'Usuarios',
+
+    'calendar'      => 'Calendario',
+    'schedule_talk' => 'Programar Discurso',
+
+    'create_new_speaker' => 'Crear Nuevo Orador',
+    'first_name'         => 'Nombres',
+    'last_name'          => 'Apellidos',
+    'congregation'       => 'Congregación',
+    'prepared_talks'     => 'Bosquejos Preparados',
+    'edit'               => 'Modificar',
+    'no_speakers'        => 'No hay oradores',
+
+    'new_speaker'           => 'Nuevo Orador',
+    'select_prepared_talks' => 'Seleccione los bosquejos preparados',
+
+    'cancel' => 'Cancelar',
+    'save'   => 'Guardar',
+
+    'edit_speaker' => 'Modificar Orador',
+
+    'create_new_talk' => 'Agregar Bosquejo',
+    'number'          => 'Número',
+    'subject'         => 'Título',
+    'not_defined'     => 'No Definido',
+    'no_talks'        => 'No hay bosquejos',
+
+    'create_new_congregation'      => 'Crear Nueva Congregación',
+    'name'                         => 'Nombre',
+    'is_group'                     => 'Es un Grupo?',
+    'public_meeting_date_and_time' => 'Fecha y Hora de la Reunión Pública',
+    'every'                        => 'Cada',
+    'at'                           => 'a las',
+    'no_congregations'             => 'No hay congregaciones',
+
+    'yes' => 'Si',
+    'no'  => 'No',
+
+    'new_congregation'  => 'Nueva Congregación',
+    'congregation_name' => 'Nombre de la Congregación',
+
+    'edit_congregation' => 'Editar Congregación',
+
+    'id'       => 'ID',
+    'is_admin' => 'Es un Administrador?',
+
+    'monday'    => 'Lunes',
+    'tuesday'   => 'Martes',
+    'wednesday' => 'Miércoles',
+    'thursday'  => 'Jueves',
+    'friday'    => 'Viernes',
+    'saturday'  => 'Sábado',
+    'sunday'    => 'Domingo',
+
+];
