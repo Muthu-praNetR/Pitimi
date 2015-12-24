0dc2c38c38f23116deb42db13ee7316bebe96217
Rubens Mariuzzo <rubens@mariuzzo.com>
2015-12-24 12:15:12 -0400

    Feature: Haitian Creole language added.

---
+++ resources/lang/ht/messages.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+
+    'app_name'        => 'Pitimi',
+    'app_description' => 'Òganizatè Diskou Piblik yo',
+
+    'login'  => 'Antre',
+    'logout' => 'Sòti',
+
+    'email'    => 'Adrès Imel',
+    'password' => 'Modepas',
+
+    'speakers'      => 'Oratè yo',
+    'talks'         => 'Plan yo',
+    'congregations' => 'Kongregasyon yo',
+    'users'         => 'Itilizatè yo',
+
+    'calendar'      => 'Kalandriye',
+    'schedule_talk' => 'Pwograme Diskou',
+
+    'create_new_speaker' => 'Kreye Nouvo Oratè',
+    'first_name'         => 'Non',
+    'last_name'          => 'Siyati',
+    'congregation'       => 'Kongregasyon',
+    'prepared_talks'     => 'Plan ki Prepare yo',
+    'edit'               => 'Modifye',
+    'no_speakers'        => 'Pa gen oratè',
+
+    'new_speaker'           => 'Nouvo Oratè',
+    'select_prepared_talks' => 'Seleksyone Plan ki Prepare yo',
+
+    'cancel' => 'Anile',
+    'save'   => 'Sere',
+
+    'edit_speaker' => 'Modifye Oratè',
+
+    'create_new_talk' => 'Kreye Nouvo Plan',
+    'number'          => 'Nimewo',
+    'subject'         => 'Sijè',
+    'not_defined'     => 'Poko Defini',
+    'no_talks'        => 'Pa gen plan',
+
+    'create_new_congregation'      => 'Kreye Nouvo Kongregasyon',
+    'name'                         => 'Non',
+    'is_group'                     => 'Èske se yon Gwoup?',
+    'public_meeting_date_and_time' => 'Jou ak Lè pou Reyinyon Piblik la',
+    'every'                        => 'Chak',
+    'at'                           => 'nan lè',
+    'no_congregations'             => 'Pa gen kongregasyon',
+
+    'yes' => 'Wi',
+    'no'  => 'Non',
+
+    'new_congregation'  => 'Nouvo Kongregasyon',
+    'congregation_name' => 'Non Kongregasyon an',
+
+    'edit_congregation' => 'Modifye Kongregason',
+
+    'id'       => 'ID',
+    'is_admin' => 'Èske se yon Administratè?',
+
+    'monday'    => 'Lendi',
+    'tuesday'   => 'Madi',
+    'wednesday' => 'Mekredi',
+    'thursday'  => 'Jedi',
+    'friday'    => 'Vandredi',
+    'saturday'  => 'Samdi',
+    'sunday'    => 'Dimanch',
+
+];
